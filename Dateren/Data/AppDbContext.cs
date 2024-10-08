94a9d62eafb6936f3eec7fa18fe4e5c11a45605f
David Frørup <dafr1@elev.tec.dk>
2024-10-08 11:05:10 +0200

    Add project files.

---
+++ Dateren/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dateren.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Dateren.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Dateren/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dateren.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+ 
+
+
+    }
+}
